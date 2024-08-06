24a2abbe2b8994f485f92b88f491057ba17df4cc
imranrizkiputranto <imranrizkiputranto@gmail.com>
2024-08-06 14:29:59 +0700

    Implemented Track listing.

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,12 +1,15 @@
 import React from "react";
 import './SearchResults.css'
+import Tracklist from "../Tracklist/Tracklist";
 
 const SearchResults = (props) => { // Create SearchResults component
 
     return ( // Returns what the component should display
         <div className="SearchResults">
             <h2>Results</h2>
-            
+            <Tracklist // Displays container of loaded tracks from API
+                tracks={props.searchResults} // pass the searchResults array from App.js into the Tracklist component
+            />
         </div>
 
     );
