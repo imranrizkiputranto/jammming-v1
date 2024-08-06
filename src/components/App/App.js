f34e1f1d193243f5239d149528df3a245b6b2f92
imranrizkiputranto <imranrizkiputranto@gmail.com>
2024-08-06 13:52:03 +0700

    Created App.js static component and style.

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+const App = () => {
+  
+  return (
+    <div>
+      <header>
+        <h1>Ja<span className='highlight'>mmm</span>ing</h1>
+      </header>
+      <div className='body'>
+
+        <div className='playlist-container'>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
